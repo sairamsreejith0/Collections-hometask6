ea7c131edd20831c65a4659316b16b825737a6d1
sairamsreejith0 <30547601+sairamsreejith0@users.noreply.github.com>
2020-03-04 17:06:42 +0530

    Add files via upload

---
+++ collections/src/main/java/com/epam/ht6/collections/Main.java
@@ -0,0 +1,57 @@
+package com.epam.ht6.collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+
+		CustomList m= new CustomList();
+	    System.out.println("Menu");
+	    System.out.println("1.Add an element to list");
+	    System.out.println("2.Remove an element from list");
+	    System.out.println("3.Display list");
+	    System.out.println("4.Exit");
+	    
+	    while(true)
+	    {
+	    	   System.out.println("Enter your choice (1-4) :");
+	    	   Scanner sc= new Scanner(System.in);
+	    	try {
+	    		int choice =sc.nextInt();
+	    		switch(choice)
+	    		{
+	    		case 1:
+	    			System.out.print("Enter an element : ");
+	    			String s1=sc.next();
+	    			String s2=sc.nextLine();
+	    			m.add(s1+s2);
+	    			break;
+	    		case 2:
+	    			m.remove();
+	    			break;
+	    		case 3:
+	    			System.out.println(m);
+	    			break;
+	    		case 4:
+	    			sc.close();
+	    			System.exit(0);
+	    			break;
+	    		default:
+	    			System.out.println("Please enter an integer between 1-4");
+	    		
+	    			
+	    		}
+	    	}
+	    	catch(InputMismatchException e)
+	    	{
+	    		System.out.println("Please enter an integer between 1-4");
+	    		
+	    	}
+	    }
+
+	}
+
+}
+
